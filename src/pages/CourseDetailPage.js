c4639d924097cdbdbcf020722fc2e3318cebf55f
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-04-13 09:31:34 +0530

    adding note page

--- src/pages/CourseDetailPage.js
+++ src/pages/CourseDetailPage.js
@@ -9,9 +9,15 @@ export default function CourseDetailPage() {
     const { courseId } = useParams();
     const [course, setCourse] = React.useState(null);
     const [loading, setLoading] = React.useState(true);
+    const [activeTab, setActiveTab] = React.useState(1);
 
     const fetchCourseDetail = async () => {
-        const query = `*[_type == "courses" && slug.current == "${courseId}"]`;
+        const query = `*[_type == "courses" && slug.current == "${courseId}"]{
+            title,
+            content,
+            url,
+            "notes": notes.asset->url
+        }`;
         const course = await client.fetch(query);
         console.log(course);
         setCourse(course[0]);
@@ -48,7 +54,28 @@ export default function CourseDetailPage() {
 
                         } title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                     </div>
-                    <div className='p-4'>
+                    
+                    <div className='flex flex-row justify-start items-center p-4 gap-4'>
+                        <button onClick={
+                            ()=>setActiveTab(1)
+                        } className={`p-2 text-lg font-bold ${activeTab === 1 ? 'text-blue-500' : 'text-gray-500'}`}>Overview</button>
+                        <button onClick={
+                            ()=>setActiveTab(2)
+                        } className={`p-2 text-lg font-bold ${activeTab === 2 ? 'text-blue-500' : 'text-gray-500'}`}>Q&A</button>
+                        <button onClick={
+                            ()=>setActiveTab(3)
+                        } className={`p-2 text-lg font-bold ${activeTab === 3 ? 'text-blue-500' : 'text-gray-500'}`}>Download</button>
+                        {/* <button onClick={
+                            ()=>setActiveTab(4)
+                        } className={`p-2 text-lg font-bold ${activeTab === 4 ? 'text-blue-500' : 'text-gray-500'}`}>
+
+                        </button> */}
+
+                    </div>
+
+                    {
+                        activeTab === 1 &&
+                        <div className='p-4'>
                         <h1 className='text-2xl font-bold'>{course && course.title}</h1>
                         <PortableText
                             content={course && course.content}
@@ -68,7 +95,39 @@ export default function CourseDetailPage() {
                             }}
                         />
 
-                    </div>
+                    </div>}
+
+                    {
+                        activeTab === 2 &&
+                        <div className='p-4'>
+                            <h1 className='text-2xl font-bold'>Q&A</h1>
+                            <div className='flex flex-row justify-start items-center p-4 gap-4'>
+                                <input type='text' className='p-2 w-3/4 border-2 border-gray-300 rounded-lg' placeholder='Ask a question' />
+                                <button className='p-2 bg-blue-500 text-white rounded-lg'>Ask</button>
+                            </div>
+                            <div className='p-4'>
+                                <div className='flex flex-row justify-start items-center gap-4'>
+                                    <img src='/images/Avatar1.jpg' alt='profile' className='w-12 h-12 rounded-full' />
+                                    <div>
+                                        <h1 className='text-lg font-bold'>John Doe</h1>
+                                        <p className='text-sm text-gray-500'>How do I get started with this course?</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    }
+
+                    {
+                        activeTab === 3 &&
+                        <div className='p-4'>
+                            <h1 className='text-2xl font-bold'>Download</h1>
+                            <div className='flex flex-row justify-start items-center gap-4'>
+                                <a target='_blank' href={`${course.notes}?dl=`}  className='p-2 bg-blue-500 text-white rounded-lg' rel="noreferrer">Download Notes</a>
+                            </div>
+                        </div>
+                    }
+
                 </div>
 
 
--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,10 +1,67 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from 'react-router-dom';
 
 import ProfileCard from "../components/ProfileCard";
 import NavCard from "../components/NavCard";
+import { useState } from "react";
 
 export default function Notes() {
+
+  const handleSubmit = async () => {
+
+    if (!note || !title) {
+      alert('Please fill in all fields');
+      return;
+    }
+
+    try {
+      const res = await fetch('http://localhost:4000/api/addnote', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('token')}`
+        },
+        body: JSON.stringify({ note, title }),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        // console.log(data);
+      }
+
+    } catch (error) {
+      console.log(error);
+
+    }
+
+  }
+  const fetchNotes = async () => {
+    try {
+      const res = await fetch('http://localhost:4000/api/notes', {
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
+      });
+      const data = await res.json();
+      console.log(data);
+      setNotes(data.notes);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+
+
+
+  const [notes, setNotes] = useState([]);
+  const [note, setNote] = useState("");
+  const [title, setTitle] = useState("");
   return (
     <>
       <div
@@ -15,55 +72,49 @@ export default function Notes() {
           <ProfileCard />
           <NavCard />
         </div>
-    <div className="mx-auto w-full px-24 flex-col justify-center align-middle gap-4 col-span-3 h-full shadow-lg mr-5">
-      <div className="w-full mb-8">
-        <h3>Type Your Notes : </h3>
-        <textarea
-          className="w-full mx-auto border-4 shadow-lg"
-          rows={5}
-        ></textarea>
-        <div className="flexgap-2 justify-center mt-4">
-          <Link to="/login" className="bg-blue-500 text-white p-2 rounded-lg">
-            Add note
-          </Link>
+        <div className="mx-auto w-full px-24 flex-col justify-center align-middle gap-4 col-span-3 h-full shadow-lg mr-5">
+          <div className="w-full mb-8">
+            <h3>Type Your Notes : </h3>
+
+            <input type="text" className="w-full my-2 p-2 mx-auto border-1 shadow-lg"
+              placeholder="Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+
+            <textarea
+              className="w-full mx-auto border-1 shadow-lg my-2"
+              value={note}
+              rows={5}
+              onChange={(e) => setNote(e.target.value)}
+              placeholder="Type your notes here"
+            ></textarea>
+            <div className="flexgap-2 justify-center mt-4">
+              <button onClick={handleSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Save
+              </button>
+            </div>
+          </div>
+
+          <div className="w-full">
+            <h3>Your Notes : </h3>
+            <div className="flex flex-col gap-4 p-4">
+              {notes.map((note) => (
+                <div key={note._id} 
+                className="border-2 p-4 rounded-lg shadow-md" 
+                >
+                  <h3 
+                  className="text-2xl font-bold"
+                  >{note.title}</h3>
+                  <p>{note.note}</p>
+                </div>
+              ))}
+            </div>
+
+          </div>
+
         </div>
       </div>
-      <div className="w-full rounded-sm shadow-lg p-4">
-        loreLorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum
-      </div>
-      <div className="w-full rounded-sm shadow-lg p-4">
-        loreLorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum
-      </div>
-      <div className="w-full rounded-sm shadow-lg p-4">
-        loreLorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum
-      </div>
-    </div>
-
-</div>
-</>
+    </>
   );
 }
