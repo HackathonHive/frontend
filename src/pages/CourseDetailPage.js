ebc577bedf72bc4c9c503bfa198ceb941c147df5
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-04-13 12:51:13 +0530

    updating file

--- src/pages/CourseDetailPage.js
+++ src/pages/CourseDetailPage.js
@@ -14,11 +14,13 @@ export default function CourseDetailPage() {
     const [activeTab, setActiveTab] = React.useState(1);
     const [question, setQuestion] = React.useState('');
     const [questions, setQuestions] = React.useState([]);
+    const [notesInput, setNotesInput] = React.useState('');
+    const [notes, setNotes] = React.useState([]);
     const navigate = useNavigate();
 
     const handleSubmitQuestion = async () => {
 
-        if(!localStorage.getItem('token')){
+        if (!localStorage.getItem('token')) {
             toast.error('Please login to ask a question');
             navigate('/login');
             return;
@@ -36,7 +38,7 @@ export default function CourseDetailPage() {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${localStorage.getItem('token')}`
                 },
-                body: JSON.stringify({ comment:question, slug:courseId }),
+                body: JSON.stringify({ comment: question, slug: courseId }),
 
             });
             const data = await res.json();
@@ -74,13 +76,13 @@ export default function CourseDetailPage() {
         }
     }
 
-                
-                
-                
+
+
+
 
     const fetchCourseDetail = async () => {
 
-        if(!localStorage.getItem('token')){
+        if (!localStorage.getItem('token')) {
             toast.error('Please login to view course details');
             navigate('/login');
             return;
@@ -98,9 +100,67 @@ export default function CourseDetailPage() {
         setLoading(false);
     }
 
+    const handleSubmitNotes = async () => {
+        if (!localStorage.getItem('token')) {
+            toast.error('Please login to write notes');
+            navigate('/login');
+            return;
+        }
+
+        if (!notesInput) {
+            alert('Please write notes');
+            return;
+        }
+
+        try {
+            const res = await fetch('http://localhost:4000/api/addnote', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                },
+                body: JSON.stringify({ note: notesInput, title: courseId }),
+
+            });
+            const data = await res.json();
+            console.log(data);
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                console.log(data);
+            }
+
+        } catch (error) {
+            console.log(error);
+
+        }
+        finally {
+            setNotesInput('');
+            fetchNotes();
+        }
+    }
+
+    const fetchNotes = async () => {
+        try {
+            const res = await fetch('http://localhost:4000/api/notes', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            });
+            const data = await res.json();
+            setNotes(data.notes);
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     React.useEffect(() => {
         fetchCourseDetail();
         fetchQuestions();
+        fetchNotes();
 
         // eslint-disable-next-line
     }, [courseId]);
@@ -131,48 +191,48 @@ export default function CourseDetailPage() {
 
                         } title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
                     </div>
-                    
+
                     <div className='flex flex-row justify-start items-center p-4 gap-4'>
                         <button onClick={
-                            ()=>setActiveTab(1)
+                            () => setActiveTab(1)
                         } className={`p-2 text-lg font-bold ${activeTab === 1 ? 'text-blue-500' : 'text-gray-500'}`}>Overview</button>
                         <button onClick={
-                            ()=>setActiveTab(2)
+                            () => setActiveTab(2)
                         } className={`p-2 text-lg font-bold ${activeTab === 2 ? 'text-blue-500' : 'text-gray-500'}`}>Q&A</button>
                         <button onClick={
-                            ()=>setActiveTab(3)
+                            () => setActiveTab(3)
                         } className={`p-2 text-lg font-bold ${activeTab === 3 ? 'text-blue-500' : 'text-gray-500'}`}>Download</button>
-                        {/* <button onClick={
-                            ()=>setActiveTab(4)
+                        <button onClick={
+                            () => setActiveTab(4)
                         } className={`p-2 text-lg font-bold ${activeTab === 4 ? 'text-blue-500' : 'text-gray-500'}`}>
-
-                        </button> */}
+                            Write Notes
+                        </button>
 
                     </div>
 
                     {
                         activeTab === 1 &&
                         <div className='p-4'>
-                        <h1 className='text-2xl font-bold'>{course && course.title}</h1>
-                        <PortableText
-                            content={course && course.content}
-                            projectId="ocpb00yf"
-                            dataset="production"
-                            className='text-gray-700 text-base font-sans'
-                            serializers={{
-                                h2: (props) => <h1
-                                className='text-3xl font-bold text-gray-800 font-semibold space-x-2'
-                                    {...props} />,
-                                li: ({ children }) => <li className="special-list-item"
-                                style={{
-                                    listStyleType: 'disc',
+                            <h1 className='text-2xl font-bold'>{course && course.title}</h1>
+                            <PortableText
+                                content={course && course.content}
+                                projectId="ocpb00yf"
+                                dataset="production"
+                                className='text-gray-700 text-base font-sans'
+                                serializers={{
+                                    h2: (props) => <h1
+                                        className='text-3xl font-bold text-gray-800 font-semibold space-x-2'
+                                        {...props} />,
+                                    li: ({ children }) => <li className="special-list-item"
+                                        style={{
+                                            listStyleType: 'disc',
+                                        }}
+                                    >{children}</li>,
+
                                 }}
-                                >{children}</li>,
+                            />
 
-                            }}
-                        />
-
-                    </div>}
+                        </div>}
 
                     {
                         activeTab === 2 &&
@@ -182,7 +242,7 @@ export default function CourseDetailPage() {
                                 <input type='text' className='p-2 w-3/4 border-2 border-gray-300 rounded-lg' placeholder='Ask a question'
                                     value={question}
                                     onChange={(e) => setQuestion(e.target.value)}
-                                 />
+                                />
                                 <button className='p-2 bg-blue-500 text-white rounded-lg'
                                     onClick={handleSubmitQuestion}
                                 >Ask</button>
@@ -212,10 +272,40 @@ export default function CourseDetailPage() {
                         <div className='p-4'>
                             <h1 className='text-2xl font-bold'>Download</h1>
                             <div className='flex flex-row justify-start items-center gap-4'>
-                                <a target='_blank' href={`${course.notes}?dl=`}  className='p-2 bg-blue-500 text-white rounded-lg' rel="noreferrer">Download Notes</a>
+                                <a target='_blank' href={`${course.notes}?dl=`} className='p-2 bg-blue-500 text-white rounded-lg' rel="noreferrer">Download Notes</a>
                             </div>
                         </div>
                     }
+
+                    {
+                        activeTab === 4 &&
+                        <div className='p-4'>
+                            <h1 className='text-2xl font-bold'>Write Notes</h1>
+                            <div className='flex flex-row justify-start items-center gap-4'>
+                                <textarea className='p-2 w-3/4 border-2 border-gray-300 rounded-lg' placeholder='Write notes'
+                                    value={notesInput}
+                                    onChange={(e) => setNotesInput(e.target.value)}
+                                />
+                                <button className='p-2 bg-blue-500 text-white rounded-lg'
+                                    onClick={handleSubmitNotes}
+                                >Save</button>
+                            </div>
+
+                            <div className='p-4'>
+                                {
+                                    notes.map((n, i) => (
+                                        <div key={i} className='p-4 w-3/4 shadow rounded-lg mb-4'>
+                                            <h1 className='text-lg font-bold'>{n.title}</h1>
+                                            <p className='text-sm text-gray-500'>{n.note}</p>
+                                        </div>
+                                    ))
+
+                                }
+                            </div>
+
+                        </div>
+                    }
+
 
                 </div>
 
